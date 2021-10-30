6fa8226914bfb49028bb19e0dbe0bb6ade86c3cb
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-10-30 18:42:33 +0200

    First commit

---
+++ Slobodan-React.js
@@ -0,0 +1,19 @@
+/* REACT is a JavaScript library for BUILDING USER INTERFACES
+
+COMPONENTS are reusable building blocks in your user interface.
+
+Components are REUSABLE and provide SEPARATION OF CONCERNS.
+
+React allows you to create re-usable and reactive components consisting of HTML, CSS and JavaScript.
+
+To run the npx create-react app ... you will need to install Node.JS from nodejs.org.
+
+Index.js is the first code file that will be executed whenever React page is loaded. This code is executed initially.
+
+REACT and REACT.DOM form REACT LIBRARY.
+
+Index.html is the single html file that will be loaded in browser.
+
+JSX code is HTML code inside JavaScript. JSX stands for JavaScript XML. It can be said that HTML in the end is XML.
+
+*/
