f46858f657b3a9140f632adbb6d9fb3616378f99
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2021-12-17 13:41:18 +0100

    Redux Demo added

---
+++ ReduxDemo/redux-demo.js
@@ -0,0 +1,29 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state; //if its not action.type === "increment" or "decrement" it will return DEFAULT STATE
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
