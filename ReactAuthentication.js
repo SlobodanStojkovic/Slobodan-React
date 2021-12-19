f175443d265220a6d7ebe8b7179b9e35fcaf3081
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2021-12-19 01:01:28 +0100

    React Authentication added

---
+++ ReactAuthentication.js
@@ -0,0 +1,34 @@
+/* 
+JWTs - JSON Web Tokens
+
+OAuth integration (example "Log in with Google")
+
+KNOWLEDGE-BASED  Authentication > Verifying users identities based on whether or not they know something:
+- Password,
+- PIN,
+- Security question.
+
+Problems: 
+- Reliant on the strength of the password,
+- Can be guessable or searchable.
+
+
+OWNERSHIP-BASED Authentication > Verifying users identities based on whether or not they HAVE something:
+- Email address,
+- Mobile phone,
+- OTP app. 
+
+Problems: 
+- Some rely indirectly on knowledge-based strategies that is, someone could guess your email password)
+- Physical devices can be stolen,
+- Physical devices can be lost.
+
+
+TWO-FACTOR Authentication > Combines more than one authentication method-usually knowledge based and ownership based.
+
+BIOLOGICAL-BASED Authentication > Verifying users identities based on hard-to fake biological characteristics.
+- Facial recognition,
+- Fingerprint readers,
+- Eye scans.
+
+*/
