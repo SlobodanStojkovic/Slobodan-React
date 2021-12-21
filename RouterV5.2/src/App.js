94580d299ae63841cc463c04ae44bd963bb5ddd9
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2021-12-21 23:12:15 +0100

    Code updated with React.lazy

--- RouterV5.2/src/App.js
+++ RouterV5.2/src/App.js
@@ -1,32 +1,47 @@
 import { Redirect, Route, Switch } from "react-router-dom";
+import React, { Suspense } from "react";
 
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
+/* import AllQuotes from "./pages/AllQuotes"; */
+/* import NewQuote from "./pages/NewQuote"; */
+/* import QuoteDetail from "./pages/QuoteDetail"; */
+/* import NotFound from "./pages/NotFound"; */
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/ui/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote")); //this will import this code only when its needed
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          {/* any path that doesnt match the above routes will match this route */}
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />{" "}
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            {/* any path that doesnt match the above routes will match this route */}
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
